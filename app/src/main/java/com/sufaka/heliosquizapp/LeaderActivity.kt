905673d6634d741fa32b67462ab56ddb1f762ead
krstkaan <k.kaanakbulak@gmail.com>
2024-04-29 00:01:41 +0300

    Leader Activity Created Main Activity Added

---
+++ app/src/main/java/com/sufaka/heliosquizapp/LeaderActivity.kt
@@ -0,0 +1,11 @@
+package com.sufaka.heliosquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leader)
+    }
+}
--- app/src/main/java/com/sufaka/heliosquizapp/MainActivity.kt
+++ app/src/main/java/com/sufaka/heliosquizapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.sufaka.heliosquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+import com.sufaka.heliosquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
+
+        val window: Window= this@MainActivity.window
+        window.statusBarColor= ContextCompat.getColor(this@MainActivity,R.color.grey)
     }
 }
